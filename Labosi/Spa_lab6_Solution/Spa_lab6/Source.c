ada93c62826276f639898afa07a0c9b1b57e27f0
JosipVuckovic <josip.vuckovic@live.com>
2019-05-15 20:05:14 +0200

    Lab 6
    Lab 6 spa

---
+++ Labosi/Spa_lab6_Solution/Spa_lab6/Source.c
@@ -0,0 +1,179 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include "book.h"
+#include "set.h"
+#include "djb2.h"
+#include "map.h"
+
+void main_menu(void);
+int zadatak_1();
+int zadatak_2();
+void do_you_want_more(void);
+
+int main(void)
+{
+	printf("******************************************************************\n");
+	printf("*    SPA Vjeba 6 2018/2019                      Josip Vuckovic   *\n");
+	printf("*    Ovaj program moze pokretati sve zadatke iz vjezbe te Vas    *\n");
+	printf("*    molim da napravite izbor koji zadatak Vas zanima.           *\n");
+	printf("******************************************************************\n");
+	main_menu();
+	return 0;
+}
+void main_menu(void)
+{
+	char izbor;
+	printf("******************************************************************\n");
+	printf("*    Grupa L1:                                                   *\n");
+	printf("*    1 - Zadatak 1 -Broj autora i knjiga                         *\n");
+	printf("*    2 - Zadatak 2 - Ispis svih autora i knjiga                  *\n");
+	printf("*    X - Izlaz                                                   *\n");
+	printf("******************************************************************\n");
+	printf("         Vas izbor: ");
+	scanf(" %c", &izbor);
+	switch (izbor)
+	{
+	case '1':
+		zadatak_1();
+		break;
+	case '2':
+		zadatak_2();
+		break;
+	case 'X':
+	case 'x':
+		printf("\nHvala na koristenju\n");
+		printf("pritisnite bilo koju tipku za izlaz\n");
+		getchar();
+		getchar();
+		break;
+	default:
+		system("cls");
+		printf("\n\n **Nepoznat odabir, molim Vas ponovite unos**\n\n");
+		main_menu();
+		break;
+	}
+}
+
+void do_you_want_more(void)
+{
+	char odabir;
+	printf("\n**********************************************************************\n");
+	printf("*Zelite li drugi zadatak?*\n");
+	printf("**********************************************************************\n");
+	printf("y/n: ");
+	scanf(" %c", &odabir);
+	switch (odabir)
+	{
+	case 'y':
+		system("cls");
+		main_menu();
+		break;
+	case 'Y':
+		system("cls");
+		main_menu();
+		break;
+	case 'n':
+		printf("\nHvala na koristenju\n");
+		printf("pritisnite bilo koju tipku za izlaz\n");
+		getchar();
+		break;
+	case 'N':
+		printf("\nHvala na koristenju\n");
+		printf("pritisnite bilo koju tipku za izlaz\n");
+		getchar();
+		break;
+	default:
+		printf("\nNepoznat unos!\n");
+		do_you_want_more();
+		break;
+	}
+}
+
+void autori_ukupno(struct book *books, unsigned broj) {
+	char autor[255+1];
+	printf("\n");
+	printf("\n**********************************************************************\n");
+	map_t knjige;
+	knjige = map_new_advanced(strcmp, djb2);
+	for (int i = 0; i < broj; ++i)
+	{
+		strcpy(autor, books[i].author);
+
+		if (!*autor)
+			return;
+
+		//pregled dali vec imamo autora i ubacivanje u mapu
+		if (map_has_key(knjige, autor))
+		{
+			map_put(knjige, _strdup(autor), map_get(knjige, autor));
+		}
+		else
+		{
+			map_put(knjige, _strdup(autor), 1);
+		}
+	}
+	printf("U datoteci je: %d knjiga te %d autora.", broj, map_size(knjige));
+}
+
+void autori_i_knjige(struct book *books, unsigned broj) {
+	char autor[255+1];
+	printf("\n");
+	printf("\n**********************************************************************\n");
+	printf("\n Ispis autora i broja njihovih knjiga: \n");
+	map_t knjige;
+	knjige = map_new_advanced(strcmp, djb2);
+	for (int i = 0; i < broj; ++i)
+	{
+		strcpy(autor, books[i].author);
+
+		if (!*autor)
+			return;
+
+		//pregled dali vec imamo autora i ubacivanje u mapu
+		if (map_has_key(knjige, autor))
+		{
+			map_put(knjige, _strdup(autor), map_get(knjige, autor) + 1);
+		}
+		else
+		{
+			map_put(knjige, _strdup(autor), 1);
+		}
+	}
+
+	for (map_iterator_t iterator = map_iterator(knjige); map_iterator_is_valid(iterator); map_iterator_next(iterator))
+	{
+		map_element_t element = map_iget(iterator);
+		printf("\nAutor: %s Br. knjiga: %d", element.key, element.value);
+	}
+}
+
+
+int zadatak_1()
+{
+
+	struct book *books = NULL;
+	unsigned broj = 0;
+
+	if (load_books("knjige.csv", &books, &broj))
+		return -1;
+
+	autori_ukupno(books, broj);
+	free(books);
+	do_you_want_more();
+	return 0;
+}
+int zadatak_2()
+{
+	struct book *books = NULL;
+	unsigned broj = 0;
+
+	if (load_books("knjige.csv", &books, &broj))
+		return -1;
+	autori_i_knjige(books, broj);
+	free(books);
+	do_you_want_more();
+	return 0;
+}
---
+++ Labosi/Spa_lab6_Solution/Spa_lab6/book.h
@@ -0,0 +1,81 @@
+#ifndef BOOK_H_INCLUDED
+
+#define BOOK_H_INCLUDED
+
+#define BOOK_NAME_MAX_LEN 80
+#define BOOK_AUTHOR_MAX_LEN	80
+
+struct book {
+	unsigned id;
+	char name[BOOK_NAME_MAX_LEN + 1];
+	char author[BOOK_AUTHOR_MAX_LEN + 1];
+	unsigned price;
+};
+
+/* ispis knjige */
+void print_book(struct book* _book);
+
+/* ucitavanje knjiga iz datoteke */
+int load_books(const char *file_name, struct book **books, unsigned *num_books);
+
+#endif /* BOOK_H_INCLUDED */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 1024
+
+void print_book(struct book* book) {
+
+	if (book) {
+		printf("id: %u, name: %s, author:%s, price:%u\n",
+			book->id, book->name, book->author, book->price);
+	}
+}
+
+int load_books(const char *file_name, struct book **books, unsigned *num_books) {
+
+	FILE *file_stream;
+	char line[MAX_LINE_SIZE];
+	struct book *pbook;
+
+	/* otvaranje datoteke */
+	file_stream = fopen(file_name, "r");
+	if (file_stream == NULL) {
+		printf("Nije moguce otvoriti datoteku %s!\n", file_name);
+		return -1;
+	}
+
+	/* brojanje knjiga, tj. redaka u datoteci */
+	*num_books = 0;
+	while (fgets(line, MAX_LINE_SIZE, file_stream))
+		++*num_books;
+
+	/* vracanje kursora na pocetak datoteke */
+	if (fseek(file_stream, 0, SEEK_SET))
+		return -2;
+
+	/* alociranje memorije */
+	if ((*books = malloc(*num_books * sizeof(struct book))) == NULL) {
+		printf("Nije moguce alocirati memoriju za %u knjiga!\n", *num_books);
+		return -3;
+	}
+
+	/* ucitavanje podataka */
+	pbook = *books;
+	while (fgets(line, MAX_LINE_SIZE, file_stream)) {
+		pbook->id = (unsigned)atoi(strtok(line, ";"));
+		strcpy(pbook->name, strtok(NULL, ";"));
+		strcpy(pbook->author, strtok(NULL, ";"));
+		pbook->price = (unsigned)atoi(strtok(NULL, ";"));
+		++pbook;
+	}
+
+	/* zatvaranje datoteke */
+	fclose(file_stream);
+
+	return 0;
+}
+
+
